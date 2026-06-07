cab35663c78794cb318df4846ae22fccd0335450
nikhi <nikhi@Nikhila>
2026-06-07 16:18:40 +0530

    Initial baseline project structure from STS.MD

---
+++ src/test/java/com/company/hrms/SpringBootEmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
